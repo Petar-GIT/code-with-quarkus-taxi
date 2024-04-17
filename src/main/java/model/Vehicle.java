a9c9a196c3edce3bfb32fd3b12b2a3011b391bb5
Pegazeusss <perokovacevic221@gmail.com>
2024-04-17 13:56:29 +0200

    Made a Vehicle class that is commented because there is no implementation of it yet

--- src/main/java/model/Vehicle.java
+++ src/main/java/model/Vehicle.java
@@ -1,4 +1,100 @@
 //package model;
 //
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.NamedQueries;
+//import jakarta.persistence.NamedQuery;
+//
+//@Entity
+//@NamedQueries({
+//        //
+//})
 //public class Vehicle {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "vehicle_seq")
+//    private Long id;
+//
+//    private String location;
+//
+//    private String licensePlateNumber;
+//
+//    private String vehicleNumber;
+//
+//    private double tariff;
+//
+//    private String model;
+//
+//
+//    public Vehicle() {
+//        super();
+//    }
+//
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getLocation() {
+//        return location;
+//    }
+//
+//    public void setLocation(String location) {
+//        this.location = location;
+//    }
+//
+//    public String getLicensePlateNumber() {
+//        return licensePlateNumber;
+//    }
+//
+//    public void setLicensePlateNumber(String licensePlateNumber) {
+//        this.licensePlateNumber = licensePlateNumber;
+//    }
+//
+//    public String getVehicleNumber() {
+//        return vehicleNumber;
+//    }
+//
+//    public void setVehicleNumber(String vehicleNumber) {
+//        this.vehicleNumber = vehicleNumber;
+//    }
+//
+//    public double getTariff() {
+//        return tariff;
+//    }
+//
+//    public void setTariff(double tariff) {
+//        this.tariff = tariff;
+//    }
+//
+//    public String getModel() {
+//        return model;
+//    }
+//
+//    public void setModel(String model) {
+//        this.model = model;
+//    }
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 //}
